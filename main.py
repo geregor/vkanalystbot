d72812006e325faa2a8cb7439f904bd1c196d836
geregor <52245105+geregor@users.noreply.github.com>
2020-05-11 00:19:00 +0300

    Add files via upload
    Добавил MySQL и расчет бота только на сегодняшний день !

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ from time import sleep
 import time
 import vk_api
 import vk
+import pymysql.cursors
+from adds import connect
 #Узнаем будущие матчи и сейчашние
 token = '8f8b935dbc09eab696c9e1b710c8bd99bbd6be6460f4177efcedac4af43f237908f17329e983683a168b8'
 
 vk = vk_api.VkApi("+375336671825", "KoLeRiNo")
 vk.auth()
+
 while True:
 
-
+    timematch = time.strftime ( "%Y-%m-%d - %A" )
     r = requests.get('https://www.hltv.org/matches')
     soup = BS(r.content, 'html.parser')
     teams = []
-    for link in soup.findAll('a'):
-        result = re.search(r'/matches/', str(link))
-        if result != None:
-            teams.append(link.get('href'))
-    #print(teams)
-    qer = list(range(10))
+    #Высчитывает команды которые играю в это день
+    for i in soup.findAll('div', class_='match-day'): #Ищет все div с статусом день матча
+        if re.search(r''+timematch+'', str(i)) != None: # Проверяет наличие дня с сегодняшней датой . Определяет!!!
+            for a in i.findAll('a'):
+                result = re.search(r'/matches/', str(a))
+                if result != None:
+                    teams.append(a.get('href'))
     i = 0
     timeforsleep = 0
     #Узнаем проценты на команду
@@ -52,20 +56,31 @@ while True:
         soup = BS ( s.content , 'html.parser' )
         for q in soup.findAll('div', class_= 'timeAndEvent'):
             timer = q.select('.time')
-            print(timer[0].text)
+            #print(timer[0].text)
             timer = timer[0].text
         #print(timer.replace(time.strftime("%H:"), ""))
         result = re.split(r':',timer,maxsplit=1)
         rusult = re.split(r':',timenow,maxsplit=1)
         #print(result,rusult)
-        text = ""
-        if teampercent[1]-teampercent[0] > 0:
-            text = text + str(teamname[1])
-        else:
-            text = text + str(teamname[0])
-        if int(result[0]) == (int(rusult[0])+3):
-                print("[БОТ]Приближается матч между "+teamname[0]+" и "+teamname[1]+"\nШансы: "+str(teampercent[0])+" на "+str(teampercent[1])+"\nОчень ожидаемо что выйграют "+text)
-                vk.method("wall.post", {"from_group": 1, "owner_id": -154885097, "message": "[БОТ]Приближается матч между "+teamname[0]+" и "+teamname[1]+"\nШансы: "+str(teampercent[0])+" на "+str(teampercent[1])+"\nОчень ожидаемо что выйграют "+text})
+        print ( str ( teamname [ 0 ] ) + "-" + str ( teamname [ 1 ] ) )
+        connection = connect()
+        with connection.cursor() as cursor :
+            resultx = cursor.execute(f"SELECT Gmatch FROM `matches` WHERE Gmatch = '{str ( teamname [ 0 ] ) + '-' + str ( teamname [ 1 ] )}' ")
+            if resultx < 1:
+                cursor.execute(f"INSERT INTO `matches`(`Gmatch`) VALUES ('{str ( teamname [ 0 ] )  +'-'+ str ( teamname [ 1 ] )}')")
+                connection.commit()
+                if int ( result [ 0 ] ) == (int ( rusult [ 0 ] )+3) :
+                    text = ""
+                    if teampercent [ 1 ] - teampercent [ 0 ] > 0 :
+                        text = text + str ( teamname [ 1 ] )
+                    else :
+                        text = text + str ( teamname [ 0 ] )
+                    print ( "[БОТ]Приближается матч между " + teamname [ 0 ] + " и " + teamname [ 1 ] + "\nШансы: " + str (
+                    teampercent [ 0 ] ) + " на " + str ( teampercent [ 1 ] ) + "\nОчень ожидаемо что выйграют " + text )
+                    vk.method("wall.post", {"from_group": 1, "owner_id": -154885097, "message": "[БОТ]Приближается матч между "+teamname[0]+" и "+teamname[1]+"\nШансы: "+str(teampercent[0])+" на "+str(teampercent[1])+"\nСкорее всего выйграют "+text})
+        #print(time.strftime("%Y-%m-%d - %A"))
+
+
 
     #Классно
         if timeforsleep == 10:
