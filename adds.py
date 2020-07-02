6bca812d5046dd4686f9854b3f70c8ee24797373
geregor <52245105+geregor@users.noreply.github.com>
2020-07-02 20:03:18 +0300

    Add files via upload

--- adds.py
+++ adds.py
@@ -1,18 +1,17 @@
 import pymysql.cursors
+from boto.s3.connection import S3Connection
+import os
 
-#Username: VcJhVl8VY9
-#Database name: VcJhVl8VY9
-#Password: 2szV2WF4BO
-#Server: remotemysql.com
-#Port: 3306
-#These are the username and password to log in to your database and phpMyAdmin
+hostt = S3Connection(os.environ['HOST'])
+usert = S3Connection(os.environ['USER'])
+passwordt = S3Connection(os.environ['PASS'])
 
 def connect():
     connection = pymysql.connect(
-        host='remotemysql.com',
-        user='VcJhVl8VY9',
-        password='2szV2WF4BO',
-        db='VcJhVl8VY9',
+        host=hostt,
+        user=usert,
+        password=passwordt,
+        db=usert,
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor)
     return connection
