979642c9be98bb0d5527b44a37938f29f12bfd9b
geregor <52245105+geregor@users.noreply.github.com>
2021-01-13 13:14:05 +0300

    Bom

--- main3.py
+++ main3.py
@@ -55,7 +55,7 @@ def main():
         lol = soup.findAll('meta')
         lol = str(lol[1])
         lol = lol.replace('<meta content="Смотри видеотрансляцию ► BETWINNER1.com и играй в LIVE! Принимаем ставки на футбол: ', '')
-        lol = lol.replace(' . Угадай победителя: , '') 
+        lol = lol.replace(' . Угадай победителя: ', '') 
         lol = lol.replace(' name="description"/>', '')
         print(lol)
 
