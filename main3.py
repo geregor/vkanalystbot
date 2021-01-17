3af4c7f0f03ab4f25d4f8fedabad0b5e69c20de7
geregor <52245105+geregor@users.noreply.github.com>
2021-01-17 22:55:19 +0300

    Add files via upload

--- main3.py
+++ main3.py
@@ -82,15 +82,17 @@ def main():
             #print(time)
             cat = ''
             c = False
-            if len(mass) == 6 and ('завершена' in time) == False:
-                if int(mass[0]) == 0 and int(mass[3]) == 0 and int(time) >= 60 and int(mass[2]) == int(mass[5]) and int(mass[2]) == 0 and c == False: #Счет 0-0 и время больше 60 минут
+            if (len(mass) == 6) and ('завершена' in time == False) and (mass[0] != '') and (mass[3] != '') and (time != '') and (mass[2] != '') and (mass[5] != ''):
+                if (int(mass[0]) == 0) and (int(mass[3]) == 0) and (int(time) >= 60) and (int(mass[2]) == int(mass[5])) and (int(mass[2]) == 0) and (c == False): #Счет 0-0 и время больше 60 минут
                     cat += 'Тотал 0.5 Б'
                     #print(33)
-               # elif int(mass[0]) != int(mass[3]) and c == False and int(time) >= 65 and int(mass[2]) == int(mass[5]) and int(mass[2]) == 0: #Счет 0-1 или 1-0 и время больше 65 минут
+               # elif (int(mass[0]) != int(mass[3])) and (c == False) and (int(time) >= 65) and (int(mass[2]) == int(mass[5])) and (int(mass[2]) == 0): #Счет 0-1 или 1-0 и время больше 65 минут
                 #    cat += 'Тотал '+str((int(mass[0])+int(mass[3])))+'.5 Б'
                     #print(22)
-                elif int(mass[0]) != int(mass[3]) and c == False and int(time) >= 55 and int(mass[2]) == int(mass[5]) and int(mass[2]) == 0:#Счет 3М и время 65 минут
+                elif (int(mass[0]) != int(mass[3])) and (c == False) and (int(time) >= 55) and (int(mass[2]) == int(mass[5])) and (int(mass[2]) == 0):#Счет 3М и время 65 минут
                     cat += 'Тотал '+str((int(mass[0])+int(mass[3])))+'.5 Б'
+                elif ('жен' in lol) == True and int(time) >= 55 :
+                    cat += 'Тотал ' + str ( (int ( mass [ 0 ] ) + int ( mass [ 3 ] )) ) + '.5 Б'
                     #print(11)
     #print(relust)
 
